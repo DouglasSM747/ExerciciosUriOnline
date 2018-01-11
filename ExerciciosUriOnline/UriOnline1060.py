948a4d9f3b48f85561a52ad2db4c9a6915dd7ffa
Douglas Silva de Melo <34309093+DouglasSM747@users.noreply.github.com>
2018-01-11 00:42:24 -0200

    Delete UriOnline1060.py

--- ExerciciosUriOnline/UriOnline1060.py
+++
@@ -1,7 +0,0 @@
-#Uri1060 (Valores positivos)
-valor_positivo = 0
-for i in range(6):
-    numero = float(input(""))
-    if (numero>=0):
-        valor_positivo+=1
-print (valor_positivo,"valores positivos")
